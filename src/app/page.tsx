ed442115e2a0226fe021371d4f5eb1995c7b6e45
Evan Luo <knacks.insects_0o@icloud.com>
2024-04-26 08:25:42 +0800

    fix: Logo color with dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@
 
 import { ModeToggle } from "@/components/mode-toggle";
 import Logo from "@/components/icons/logo";
-import { useTheme } from "next-themes";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
@@ -12,16 +11,15 @@ import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 
 const formSchema = z.object({
-  ironpulseId: z.string().email(),
+  ironPulseId: z.string().email(),
   password: z.string().min(5).max(70)
 })
 
 export default function Home() {
-  const { theme } = useTheme()
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      ironpulseId: "",
+      ironPulseId: "",
       password: ""
     },
   })
@@ -33,12 +31,12 @@ export default function Home() {
   return (
     <>
       <header className="top-0 m-2 w-full h-14 flex flex-row items-center space-x-3">
-        <Logo className="w-20 h-20 pl-6" fill={theme === "light" ? "black" : "white"}></Logo>
+        <Logo className="w-20 h-20 pl-6" fill="fill-primary"></Logo>
         <h1 className="text-xl font-bold">IronPulse SSO</h1>
         <ModeToggle></ModeToggle>
       </header>
       <main>
-        <Card className="w-[400px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <Card className="w-[350px] lg:w-[400px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <CardHeader>
             <CardTitle>Login</CardTitle>
             <CardDescription>Sign in through your IronPulse ID</CardDescription>
@@ -48,7 +46,7 @@ export default function Home() {
               <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
                 <FormField
                   control={form.control}
-                  name="ironpulseId"
+                  name="ironPulseId"
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>IronPulse ID</FormLabel>
