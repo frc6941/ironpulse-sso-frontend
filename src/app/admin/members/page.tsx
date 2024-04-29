269e4a199d92fa800e7ec88a03b723c154d1109e
Ael <aelzimmer@gmail.com>
2024-04-29 19:30:28 +0800

    refactor: admin layout.

--- src/app/admin/members/page.tsx
+++ src/app/admin/members/page.tsx
@@ -1,13 +1,8 @@
 'use client'
 
-import AdminNavBar from "@/components/admin-nav";
-
 export default function Members() {
   return (
     <>
-      <header className="top-0 w-full">
-        <AdminNavBar current="members"></AdminNavBar>
-      </header>
     </>
   )
 }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,8 @@
 'use client'
 
-import AdminNavBar from "@/components/admin-nav";
-
 export default function Admin() {
   return (
     <>
-      <header className="top-0 w-full">
-        <AdminNavBar current="overview"></AdminNavBar>
-      </header>
       <main>
         <div className="flex flex-row">
           <h1 className="p-8 text-3xl font-bold">Overview</h1>
